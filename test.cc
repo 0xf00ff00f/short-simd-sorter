9964efc721ef1ec9d4121f2520b45042a6a34ca2
Mauro Persano <fzorto@gmail.com>
2020-09-19 07:45:10 -0300

    Removed benchmarking
    Wasn't really benchmarking anything.

--- test.cc
+++ test.cc
@@ -1,15 +1,11 @@
-#include <algorithm>
 #include <array>
 #include <cassert>
-#include <chrono>
 #include <functional>
 #include <iostream>
 #include <numeric>
 
-unsigned test(const std::function<void(std::array<float, 8> &)> &sort)
+void test(const std::function<void(std::array<float, 8> &)> &sort)
 {
-    const auto start = std::chrono::steady_clock::now();
-
     std::array<float, 8> arr;
     std::iota(arr.begin(), arr.end(), 0);
     do {
@@ -19,15 +15,9 @@ unsigned test(const std::function<void(std::array<float, 8> &)> &sort)
         for (float n : temp)
             std::cout << n << ' ';
         std::cout << '\n';
-        assert(std::is_sorted(temp.begin(), temp.end()));
 #endif
+        assert(std::is_sorted(temp.begin(), temp.end()));
     } while (std::next_permutation(arr.begin(), arr.end()));
-
-    const auto finish = std::chrono::steady_clock::now();
-    const auto elapsed =
-            std::chrono::duration_cast<std::chrono::milliseconds>(finish - start)
-                    .count();
-    return static_cast<unsigned>(elapsed);
 }
 
 extern void sort_bitonic1(std::array<float, 8> &arr);
@@ -35,9 +25,6 @@ extern void sort_bitonic2(std::array<float, 8> &arr);
 
 int main()
 {
-    std::cout << "bitonic1: " << test(sort_bitonic1) << " ms\n";
-    std::cout << "bitonic2: " << test(sort_bitonic2) << " ms\n";
-    std::cout << "std::sort: " << test([](std::array<float, 8> &arr) {
-        std::sort(arr.begin(), arr.end());
-    }) << " ms\n";
+    test(sort_bitonic1);
+    test(sort_bitonic2);
 }
